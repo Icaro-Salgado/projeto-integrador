07330cc9afa440311dd8c7bc741f2c34182f846a
pedroLSoares <pedro.levada@mercadolivre.com>
2022-04-22 16:20:59 -0300

    feat/req01 - Section service and models
    open #7

--- src/main/java/br/com/mercadolivre/projetointegrador/ProjetoIntegradorApplication.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/ProjetoIntegradorApplication.java
@@ -2,8 +2,10 @@ package br.com.mercadolivre.projetointegrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ProjetoIntegradorApplication {
 
     public static void main(String[] args) {
---
+++ src/test/java/br/com/mercadolivre/projetointegrador/test_utils/SectionServiceTestUtils.java
@@ -0,0 +1,38 @@
+package br.com.mercadolivre.projetointegrador.test_utils;
+
+import br.com.mercadolivre.projetointegrador.warehouse.model.Location;
+import br.com.mercadolivre.projetointegrador.warehouse.model.Section;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.Date;
+
+public class SectionServiceTestUtils {
+
+    public static Section getMockSection(){
+        Calendar calendar  = Calendar.getInstance();
+        calendar.set(2022, Calendar.JANUARY, 1);
+
+        return Section
+                .builder()
+                .id(1L)
+                .warehouse("wh1")
+                .manager("m1")
+                .location(
+                        new Location(
+                                "Brazil",
+                                "SP",
+                                "São Paulo",
+                                "neigh",
+                                "street",
+                                11,
+                                13134123
+                        )
+                )
+                .maximumTemperature(BigDecimal.valueOf(25.33))
+                .minimumTemperature(BigDecimal.valueOf(15.333))
+                .capacity(100)
+                .createdAt(calendar.getTime())
+                .build();
+    }
+}
