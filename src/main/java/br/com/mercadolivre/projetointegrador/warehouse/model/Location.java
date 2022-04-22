53884ae93332c3d3ad92eba28a9541008953c194
pedroLSoares <pedro.levada@mercadolivre.com>
2022-04-22 17:09:42 -0300

    feat/r01 add missing getter and setter into location entity

--- src/main/java/br/com/mercadolivre/projetointegrador/warehouse/model/Location.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/warehouse/model/Location.java
@@ -1,13 +1,17 @@
 package br.com.mercadolivre.projetointegrador.warehouse.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Location {
 
     @Id
