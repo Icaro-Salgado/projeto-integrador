c22be4c86691701a26764a6f17bbd5c760adb554
Icaro Gabriel Pereira Salgado <icaro.salgado@mercadolivre.com>
2022-04-22 18:41:58 -0300

    feat: add integration to created services and models

--- src/main/java/br/com/mercadolivre/projetointegrador/warehouse/controller/InboundOrderController.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/warehouse/controller/InboundOrderController.java
@@ -1,17 +1,29 @@
 package br.com.mercadolivre.projetointegrador.warehouse.controller;
 
 import br.com.mercadolivre.projetointegrador.warehouse.dto.request.InboundOrderDTO;
+import br.com.mercadolivre.projetointegrador.warehouse.mapper.InboundOrderMapper;
+import br.com.mercadolivre.projetointegrador.warehouse.model.InboundOrder;
+import br.com.mercadolivre.projetointegrador.warehouse.service.WarehouseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/inboundorder")
+@RequiredArgsConstructor
 public class InboundOrderController {
+    private final WarehouseService warehouseService;
+    private final InboundOrderMapper inboundOrderMapper = InboundOrderMapper.INSTANCE;
 
     @PostMapping
-    public ResponseEntity<?> addInboundOrder(@RequestBody InboundOrderDTO dto) {
-        // TODO: Converter DTO para model
-        // TODO: Salvar o InboundOrder
+    // TODO: EntityModel<?> -> EntityModel<List<SavedBatches>>
+    public EntityModel<?> addInboundOrder(@RequestBody InboundOrderDTO dto) {
+        InboundOrder inboundOrderToSave = inboundOrderMapper.toModel(dto);
+        List<Object> savedBatches = warehouseService.saveBatchInSection(inboundOrderToSave);
+
         // TODO: Converter o retorno para DTO
         // TODO: Montar o EntityModel
         // TODO: Retornar o created com a URI
@@ -20,8 +32,9 @@ public class InboundOrderController {
 
     // Aqui podemos fazer um put em /{id} ou na raiz, se for na raíz o ID tem que vir no DTO
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateInboundOrder(@RequestBody InboundOrderDTO dto, @PathVariable String id) {
-        // TODO: Converte DTO para Model
+    // TODO: EntityModel<?> -> EntityModel<SavedBatch>
+    public EntityModel<?> updateInboundOrder(@RequestBody InboundOrderDTO dto, @PathVariable String id) {
+        InboundOrder inboundOrderToUpdate = inboundOrderMapper.toModel(dto);
         // TODO: Atualizar o InboundOrder
         // TODO: Converter o retorno para DTO
         // TODO: Montar o EntityModel
