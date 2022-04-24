5545d7d1f73452c8c6e09a2dc8578379649ffa3d
pedroLSoares <pedro.levada@mercadolivre.com>
2022-04-24 14:18:42 -0300

    feat/r01 Removing unused method from mapper

--- src/main/java/br/com/mercadolivre/projetointegrador/warehouse/mapper/InboundOrderMapper.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/warehouse/mapper/InboundOrderMapper.java
@@ -1,13 +1,10 @@
 package br.com.mercadolivre.projetointegrador.warehouse.mapper;
 
-import br.com.mercadolivre.projetointegrador.warehouse.dto.request.InboundOrderDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-
+@Mapper
 public interface InboundOrderMapper {
 
     InboundOrderMapper INSTANCE = Mappers.getMapper(InboundOrderMapper.class);
-
-    InboundOrderDTO toDTO();
 }
