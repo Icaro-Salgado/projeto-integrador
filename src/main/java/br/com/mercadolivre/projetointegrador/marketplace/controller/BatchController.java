972b00b7ca9d2b7b93033ee9d9baf6c980ddbfbd
esutil <evandro.sutil@mercadolivre.com>
2022-04-25 16:10:21 -0300

    feature: add findById and update to Batch

--- src/main/java/br/com/mercadolivre/projetointegrador/marketplace/controller/BatchController.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/marketplace/controller/BatchController.java
@@ -1,6 +1,7 @@
 package br.com.mercadolivre.projetointegrador.marketplace.controller;
 
 import br.com.mercadolivre.projetointegrador.marketplace.dto.CreateOrUpdateBatchDTO;
+import br.com.mercadolivre.projetointegrador.marketplace.exception.NotFoundException;
 import br.com.mercadolivre.projetointegrador.marketplace.model.Batch;
 import br.com.mercadolivre.projetointegrador.marketplace.service.BatchService;
 import lombok.AllArgsConstructor;
@@ -35,5 +36,18 @@ public class BatchController {
     @GetMapping
     public ResponseEntity<List<Batch>> getAll() {
         return ResponseEntity.ok(batchService.findAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateBatch(
+            @PathVariable Long id,
+            @RequestBody Batch batch,
+            UriComponentsBuilder uriBuilder) throws NotFoundException {
+
+        batchService.updateBatch(id, batch);
+
+        URI uri = uriBuilder.path("/api/v1/batches/{id}").buildAndExpand(batch.getId()).toUri();
+
+        return ResponseEntity.noContent().location(uri).build();
     }
 }
--- src/main/java/br/com/mercadolivre/projetointegrador/marketplace/service/BatchService.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/marketplace/service/BatchService.java
@@ -1,11 +1,13 @@
 package br.com.mercadolivre.projetointegrador.marketplace.service;
 
+import br.com.mercadolivre.projetointegrador.marketplace.exception.NotFoundException;
 import br.com.mercadolivre.projetointegrador.marketplace.model.Batch;
 import br.com.mercadolivre.projetointegrador.marketplace.repository.BatchRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -17,5 +19,29 @@ public class BatchService {
 
     public List<Batch> findAll() {
         return batchRepository.findAll();
+    }
+
+    public Batch findById(Long id) throws NotFoundException {
+        Optional<Batch> optionalBatch = batchRepository.findById(id);
+        if (optionalBatch.isEmpty()) {
+            throw new NotFoundException("Lote não encontrado");
+        }
+        return optionalBatch.get();
+    }
+
+    public void updateBatch(Long id, Batch updatedBatch) throws NotFoundException {
+        Batch batch = findById(id);
+
+        batch.setBatch_number(updatedBatch.getBatch_number());
+        batch.setPrice(updatedBatch.getPrice());
+        batch.setDue_date(updatedBatch.getDue_date());
+        batch.setManufacturing_datetime(updatedBatch.getManufacturing_datetime());
+        batch.setProduct(updatedBatch.getProduct());
+        batch.setOrder_number(updatedBatch.getOrder_number());
+        batch.setSection_id(updatedBatch.getSeller_id());
+        batch.setSeller_id(updatedBatch.getSeller_id());
+        batch.setQuantity(updatedBatch.getQuantity());
+
+        batchRepository.save(batch);
     }
 }
