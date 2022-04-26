cf05d628b2a2b8dcd13efb15c29917c6295b9dc8
Icaro Gabriel Pereira Salgado <icaro.salgado@mercadolivre.com>
2022-04-26 10:28:58 -0300

    test: configure warehouse service tests

--- src/test/java/br/com/mercadolivre/projetointegrador/unit/service/WarehouseServiceTests.java
+++ src/test/java/br/com/mercadolivre/projetointegrador/unit/service/WarehouseServiceTests.java
@@ -1,8 +1,18 @@
 package br.com.mercadolivre.projetointegrador.unit.service;
 
+import br.com.mercadolivre.projetointegrador.warehouse.repository.WarehouseRepository;
+import br.com.mercadolivre.projetointegrador.warehouse.service.WarehouseService;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 public class WarehouseServiceTests {
+    @Mock
+    private WarehouseRepository warehouseRepository;
+
+    @InjectMocks
+    private WarehouseService warehouseService;
+
     @Test
     public void TestIfReturnsRightBatchesOnFindProduct() {
         assert false;
