7804ed67391bb4436e3f4cd776abc510e05c173a
Icaro Gabriel Pereira Salgado <icaro.salgado@mercadolivre.com>
2022-04-26 08:03:37 -0300

    feat: add created batch DTO

---
+++ src/main/java/br/com/mercadolivre/projetointegrador/warehouse/dto/response/CreatedBatchResponseDTO.java
@@ -0,0 +1,61 @@
+package br.com.mercadolivre.projetointegrador.warehouse.dto.response;
+
+import br.com.mercadolivre.projetointegrador.marketplace.model.Batch;
+import br.com.mercadolivre.projetointegrador.marketplace.model.Product;
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Builder
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreatedBatchResponseDTO {
+
+    private Long id;
+
+    private Product product;
+    private Long section_id;
+    private Long seller_id;
+    private BigDecimal price;
+    private Integer order_number;
+    private Integer batch_number;
+    private Integer quantity;
+    private LocalDate manufacturing_datetime;
+    private LocalDate due_date;
+    private LocalDate created_at;
+
+    public Batch toModel() {
+        return Batch
+                .builder()
+                .product(this.product)
+                .section_id(this.section_id)
+                .seller_id(this.seller_id)
+                .price(this.price)
+                .order_number(this.order_number)
+                .batch_number(this.batch_number)
+                .quantity(this.quantity)
+                .manufacturing_datetime(this.manufacturing_datetime)
+                .due_date(this.due_date)
+                .created_at(this.created_at)
+                .build();
+    }
+
+    public static CreatedBatchResponseDTO fromModel(Batch batch) {
+        return CreatedBatchResponseDTO
+                .builder()
+                .product(batch.getProduct())
+                .section_id(batch.getSection_id())
+                .seller_id(batch.getSeller_id())
+                .price(batch.getPrice())
+                .order_number(batch.getOrder_number())
+                .batch_number(batch.getBatch_number())
+                .quantity(batch.getQuantity())
+                .manufacturing_datetime(batch.getManufacturing_datetime())
+                .due_date(batch.getDue_date())
+                .created_at(batch.getCreated_at())
+                .build();
+    }
+}
