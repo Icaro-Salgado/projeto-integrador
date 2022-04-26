3ad0d2eb4302d237be575cb876cacd64873c91fc
Icaro Gabriel Pereira Salgado <icaro.salgado@mercadolivre.com>
2022-04-26 08:33:49 -0300

    refact: optmizing imports

--- src/main/java/br/com/mercadolivre/projetointegrador/warehouse/assembler/BatchAssembler.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/warehouse/assembler/BatchAssembler.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component
 public class BatchAssembler {
