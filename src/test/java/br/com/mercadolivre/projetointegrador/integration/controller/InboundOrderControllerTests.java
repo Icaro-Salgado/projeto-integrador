8d9a0108f897ae96fddaee0ea33e7557081e89d8
Icaro Gabriel Pereira Salgado <icaro.salgado@mercadolivre.com>
2022-04-27 17:20:57 -0300

    refact: improve inbound order creation test

--- src/test/java/br/com/mercadolivre/projetointegrador/integration/controller/InboundOrderControllerTests.java
+++ src/test/java/br/com/mercadolivre/projetointegrador/integration/controller/InboundOrderControllerTests.java
@@ -6,10 +6,12 @@ import br.com.mercadolivre.projetointegrador.marketplace.repository.ProductRepos
 import br.com.mercadolivre.projetointegrador.test_utils.SectionServiceTestUtils;
 import br.com.mercadolivre.projetointegrador.warehouse.dto.request.CreateBatchPayloadDTO;
 import br.com.mercadolivre.projetointegrador.warehouse.dto.request.InboundOrderDTO;
+import br.com.mercadolivre.projetointegrador.warehouse.dto.response.CreatedBatchDTO;
 import br.com.mercadolivre.projetointegrador.warehouse.model.InboundOrder;
 import br.com.mercadolivre.projetointegrador.warehouse.model.Section;
 import br.com.mercadolivre.projetointegrador.warehouse.repository.SectionRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
@@ -44,7 +47,7 @@ public class InboundOrderControllerTests {
 
     @Test
     public void TestIfInboundOrderIsCreated() throws Exception {
-
+        // SETUP
         Section mockSection = SectionServiceTestUtils.getMockSection();
         sectionRepository.save(mockSection);
 
@@ -67,18 +70,50 @@ public class InboundOrderControllerTests {
 
         String payload = new ObjectMapper().writeValueAsString(objPayload);
 
-        MvcResult mvcResult = mockMvc.perform(
+        // ACT AND ASSERT
+        MvcResult postResult = mockMvc.perform(
                 MockMvcRequestBuilders
                         .post(INBOUND_URL)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(payload))
                 .andExpect(MockMvcResultMatchers.status().isCreated()).andReturn();
 
-        System.out.println(mvcResult.getResponse().getContentAsString());
+        String link = JsonPath.read(postResult.getResponse().getContentAsString(), "$.[0]links[0][\"self\"]");
+
+        mockMvc.perform(MockMvcRequestBuilders.get(link)).andExpect(MockMvcResultMatchers.status().isOk()).andReturn();
     }
 
     @Test
-    public void TestIfInboundOrderIsUpdated() {
-        assert true;
+    public void TestIfInboundOrderIsUpdated() throws Exception {
+        // SETUP
+        Section mockSection = SectionServiceTestUtils.getMockSection();
+        sectionRepository.save(mockSection);
+
+
+        Product productMock = new Product(1L, "teste", "teste", null);
+        productRepository.save(productMock);
+
+        CreateBatchPayloadDTO batchMock = CreateBatchPayloadDTO
+                .builder()
+                .product(productMock)
+                .build();
+
+        InboundOrderDTO objPayload = InboundOrderDTO
+                .builder()
+                .orderNumber(1)
+                .batches(List.of(batchMock))
+                .sectionCode(1)
+                .warehouseCode(1L)
+                .build();
+
+        String payload = new ObjectMapper().writeValueAsString(objPayload);
+
+        // ACT AND ASSERT
+        MvcResult mvcResult = mockMvc.perform(
+                MockMvcRequestBuilders
+                        .put(INBOUND_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(payload))
+                .andExpect(MockMvcResultMatchers.status().isCreated()).andReturn();
     }
 }
