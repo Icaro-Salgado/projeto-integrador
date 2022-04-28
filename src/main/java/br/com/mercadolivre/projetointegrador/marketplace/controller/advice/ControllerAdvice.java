36d528d71f1f03a97d4793ef407bf3b8d71a6124
pedroLSoares <pedro.levada@mercadolivre.com>
2022-04-28 18:49:43 -0300

    feat/r01 add not found message

--- src/main/java/br/com/mercadolivre/projetointegrador/marketplace/controller/advice/ControllerAdvice.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/marketplace/controller/advice/ControllerAdvice.java
@@ -4,22 +4,26 @@ import br.com.mercadolivre.projetointegrador.marketplace.exception.ErrorDTO;
 import br.com.mercadolivre.projetointegrador.marketplace.exception.InvalidCategoryException;
 import br.com.mercadolivre.projetointegrador.marketplace.exception.NotFoundException;
 import br.com.mercadolivre.projetointegrador.marketplace.exception.ProductAlreadyExists;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdvice {
-  @ExceptionHandler(NotFoundException.class)
-  public ResponseEntity<Void> notFoundHandler(Exception e) {
-    return ResponseEntity.notFound().build();
-  }
-
   @ExceptionHandler(value = {InvalidCategoryException.class, ProductAlreadyExists.class})
   public ResponseEntity<ErrorDTO> badRequestParams(Exception e) {
     ErrorDTO error = new ErrorDTO();
     error.setError("Parâmetros inválidos.");
     error.setMessage(e.getMessage());
     return ResponseEntity.badRequest().body(error);
+  }
+
+  @ExceptionHandler(value = NotFoundException.class)
+  public ResponseEntity<ErrorDTO> notFoundHandler(NotFoundException e) {
+    ErrorDTO error = new ErrorDTO();
+    error.setError("Objeto não encontrado.");
+    error.setMessage(e.getMessage());
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
   }
 }
