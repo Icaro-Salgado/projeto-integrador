5894795afc4c3f6145a554f7495c8a70a60d43aa
esutil <evandro.sutil@mercadolivre.com>
2022-04-29 19:06:44 -0300

    feature: change DTO used in CartController

--- src/main/java/br/com/mercadolivre/projetointegrador/marketplace/dtos/PurchaseOrderDTO.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/marketplace/dtos/PurchaseOrderDTO.java
@@ -1,23 +1,28 @@
 package br.com.mercadolivre.projetointegrador.marketplace.dtos;
 
+import br.com.mercadolivre.projetointegrador.marketplace.enums.CartStatusCodeEnum;
 import br.com.mercadolivre.projetointegrador.marketplace.model.Cart;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class PurchaseOrderDTO extends Cart {
-  private Long id;
+public class PurchaseOrderDTO {
+
+  private List<CartProductDTO> products;
 
   public Cart mountCart() {
     Cart cart = new Cart();
-    cart.setDate(super.getDate());
-    cart.setStatusCode(super.getStatusCode());
-    cart.setProducts(super.getProducts());
+    cart.setDate(LocalDate.now());
+    cart.setStatusCode(CartStatusCodeEnum.ABERTO);
+    cart.setProducts(this.products);
     return cart;
   }
 }
