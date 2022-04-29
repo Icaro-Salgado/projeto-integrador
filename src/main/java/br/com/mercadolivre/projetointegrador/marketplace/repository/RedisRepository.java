ecd60f3799781a86502829e040ae329ea7cfc075
esutil <evandro.sutil@mercadolivre.com>
2022-04-29 15:15:57 -0300

    feature: initial cart controller

--- src/main/java/br/com/mercadolivre/projetointegrador/marketplace/repository/RedisRepository.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/marketplace/repository/RedisRepository.java
@@ -1,18 +1,27 @@
 package br.com.mercadolivre.projetointegrador.marketplace.repository;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Repository;
 import redis.clients.jedis.Jedis;
 
+@AllArgsConstructor
+@Repository
 public class RedisRepository implements CacheRepository<String, String> {
 
     Jedis jedis;
 
     @Override
     public String get(String key) {
-        return null;
+        return jedis.get(key);
     }
 
     @Override
     public void set(String key, String value) {
+        jedis.set(key, value);
+    }
 
+    public void setEx(String key, Long ttl, String value) {
+        jedis.setex(key, ttl, value);
     }
 }
