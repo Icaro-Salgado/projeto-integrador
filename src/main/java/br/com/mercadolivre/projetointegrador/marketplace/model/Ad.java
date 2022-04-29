40316ff7e9e87f34f1db3a3f6921eb149b5ce296
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-04-29 12:03:35 -0300

    feat: implement ad and ad_batch entities with JPA

--- src/main/java/br/com/mercadolivre/projetointegrador/marketplace/model/Ad.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/marketplace/model/Ad.java
@@ -4,23 +4,42 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
+@Entity
 @Getter @Setter
 @NoArgsConstructor
 public class Ad {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private List<Long> batches;
-    private Long seller_id;
+
+    @Column(name = "seller_id")
+    private Long sellerId;
+
+    @Column
     private String name;
+
+    @Column
     private int quantity;
+
+    @Column
     private BigDecimal price;
+
+    @Column
     private int discount;
+
+    @Column
     private String category;
+
+    @Column
     private LocalDate manufacturing_date;
+
+    @Column
     private LocalDate due_date;
 
 }
---
+++ src/main/java/br/com/mercadolivre/projetointegrador/marketplace/model/AdBatch.java
@@ -0,0 +1,25 @@
+package br.com.mercadolivre.projetointegrador.marketplace.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "ad_batches")
+@Getter @Setter
+@NoArgsConstructor
+public class AdBatch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ad_id", referencedColumnName = "id", nullable = false)
+    private Ad ads;
+
+    @Column(name = "batch_id")
+    private Long batchId;
+
+}
