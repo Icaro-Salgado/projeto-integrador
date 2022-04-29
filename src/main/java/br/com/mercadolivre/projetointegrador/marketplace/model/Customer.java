1a9d7080a033a485e5a2f3fcb31145bd299cb8bb
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-04-29 12:04:18 -0300

    feat: add customer entity on JPA

--- src/main/java/br/com/mercadolivre/projetointegrador/marketplace/model/Customer.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/marketplace/model/Customer.java
@@ -4,13 +4,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
+@Entity
 @Getter @Setter
 @NoArgsConstructor
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
     private String name;
+
+    @Column
     private String email;
+
+    @Column
     private String password;
 
 }
