17f9897d382f3ee25161265305e1f25ce043b316
esutil <evandro.sutil@mercadolivre.com>
2022-04-29 19:20:53 -0300

    feature: contains and switch methods to `CartStatusCodeEnum`

--- src/main/java/br/com/mercadolivre/projetointegrador/marketplace/enums/CartStatusCodeEnum.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/marketplace/enums/CartStatusCodeEnum.java
@@ -3,4 +3,27 @@ package br.com.mercadolivre.projetointegrador.marketplace.enums;
 public enum CartStatusCodeEnum {
   ABERTO,
   FINALIZADO;
+
+  public static boolean contains(String status) {
+    for (CartStatusCodeEnum c : CartStatusCodeEnum.values()) {
+      if (c.name().equals(status)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public static CartStatusCodeEnum switchStatus(CartStatusCodeEnum cartStatus) {
+    CartStatusCodeEnum statusCodeEnum = null;
+    switch (cartStatus) {
+      case ABERTO:
+        statusCodeEnum = CartStatusCodeEnum.FINALIZADO;
+      break;
+      case FINALIZADO:
+        statusCodeEnum = CartStatusCodeEnum.ABERTO;
+      break;
+    }
+    return statusCodeEnum;
+  }
 }
