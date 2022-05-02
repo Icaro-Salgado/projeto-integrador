30ebc03dbff5317ea40b313c9912e1e0eb320d25
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-05-02 12:06:00 -0300

    feat: implement create, list and delete services and controller to ad

--- src/main/java/br/com/mercadolivre/projetointegrador/marketplace/controllers/AdController.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/marketplace/controllers/AdController.java
@@ -1,9 +1,17 @@
 package br.com.mercadolivre.projetointegrador.marketplace.controllers;
 
+import br.com.mercadolivre.projetointegrador.marketplace.dtos.CreateOrUpdateAdDTO;
+import br.com.mercadolivre.projetointegrador.marketplace.exceptions.NotFoundException;
+import br.com.mercadolivre.projetointegrador.marketplace.exceptions.UnauthorizedException;
 import br.com.mercadolivre.projetointegrador.marketplace.model.Ad;
 import br.com.mercadolivre.projetointegrador.marketplace.services.AdService;
+import br.com.mercadolivre.projetointegrador.marketplace.utils.IdentifyCustomer;
+import br.com.mercadolivre.projetointegrador.warehouse.model.AppUser;
+import br.com.mercadolivre.projetointegrador.warehouse.repository.AppUserRepository;
+import br.com.mercadolivre.projetointegrador.warehouse.service.TokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -12,23 +20,61 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/ad")
+@RequestMapping("/api/v1/ads")
 @AllArgsConstructor
 public class AdController {
 
   AdService adService;
+  AppUserRepository tokenService;
 
   @PostMapping
-  public ResponseEntity<Void> createAd(@Valid @RequestBody Ad ad, UriComponentsBuilder uriBuilder) {
-    adService.createAd(ad);
+  public ResponseEntity<Void> createAd(
+          @Valid @RequestBody CreateOrUpdateAdDTO createOrUpdateAdDTO,
+          UriComponentsBuilder uriBuilder,
+          Authentication authentication
+  ) {
+    AppUser requestUser = (AppUser) authentication.getPrincipal();
+    Ad ad = adService.createAd(requestUser.getId(), createOrUpdateAdDTO);
     URI uri = uriBuilder.path("/api/v1/ad/{id}").buildAndExpand(ad.getId()).toUri();
 
     return ResponseEntity.created(uri).build();
   }
 
-  @GetMapping("/seller/{seller_id}")
-  public ResponseEntity<List<Ad>> listCustomerAds(@PathVariable Long seller_id) {
-    List<Ad> sellerAds = adService.listAdsByCustomerId(seller_id);
+  @GetMapping("/{id}")
+  public ResponseEntity<Ad> findAd(
+          @PathVariable Long id
+  ) {
+    return ResponseEntity.ok(adService.findAdById(id));
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Ad>> listAds(
+          @RequestParam(required = false) String name
+  ) {
+    if (name != null) {
+      return ResponseEntity.ok(adService.listAds(name));
+    }
+
+    return ResponseEntity.ok(adService.listAds());
+  }
+
+  @GetMapping("/seller")
+  public ResponseEntity<List<Ad>> listCustomerAds(
+          Authentication authentication) {
+    AppUser requestUser = (AppUser) authentication.getPrincipal();
+    List<Ad> sellerAds = adService.listAdsByCustomerId(requestUser.getId());
+
     return ResponseEntity.ok(sellerAds);
+  }
+
+  @DeleteMapping("/{adId}/delete")
+  public ResponseEntity<Void> deleteAd(
+          @PathVariable Long adId,
+          Authentication authentication
+  ) throws UnauthorizedException {
+    AppUser requestUser = (AppUser) authentication.getPrincipal();
+    adService.deleteAd(requestUser.getId(), adId);
+
+    return ResponseEntity.noContent().build();
   }
 }
