70802c931dd8df40f3182de2540f387818ec727f
Klinton Sae Hyung Lee <klinton.lee@mercadolivre.com>
2022-05-02 12:08:55 -0300

    feat: implement purchase and adpurchase models

---
+++ src/main/java/br/com/mercadolivre/projetointegrador/marketplace/model/Purchase.java
@@ -0,0 +1,21 @@
+package br.com.mercadolivre.projetointegrador.marketplace.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class Purchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "buyer_id")
+    private Long buyerId;
+
+}
