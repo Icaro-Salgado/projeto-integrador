6fbd7c341301f36ac18a9a376a1b750180eca4e4
Paulorlima <paulo.alima@mercadolivre.com>
2022-05-03 15:29:09 -0300

    add response batchDTO

--- src/main/java/br/com/mercadolivre/projetointegrador/warehouse/controller/WarehouseController.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/warehouse/controller/WarehouseController.java
@@ -46,91 +46,92 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @Tag(name = "Warehouse")
 public class WarehouseController {
 
-  private final WarehouseService warehouseService;
-  private final WarehouseAssembler assembler;
-  private final SectionAssembler sectionAssembler;
-  private final BatchAssembler batchAssembler;
+    private final WarehouseService warehouseService;
+    private final WarehouseAssembler assembler;
+    private final SectionAssembler sectionAssembler;
+    private final BatchAssembler batchAssembler;
 
-  @Operation(summary = "CRIA UM NOVO ARMAZÉM", description = "Cria um novo armazém/depósito ")
-  @ApiResponses(
-      value = {
-        @ApiResponse(
-            responseCode = "200",
-            description = "Armazém criado com sucesso",
-            content = {
-              @Content(
-                  mediaType = "application/json",
-                  schema = @Schema(implementation = CreateWarehousePayloadDTO.class))
-            }),
-        @ApiResponse(
-            responseCode = "400",
-            description = "Dados invalidos!",
-            content = {
-              @Content(
-                  mediaType = "application/json",
-                  schema = @Schema(implementation = ErrorDTO.class))
+    @Operation(summary = "CRIA UM NOVO ARMAZÉM", description = "Cria um novo armazém/depósito ")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Armazém criado com sucesso",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = CreateWarehousePayloadDTO.class))
+                            }),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Dados invalidos!",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = ErrorDTO.class))
+                            })
             })
-      })
-  @PostMapping
-  public ResponseEntity<WarehouseResponseDTO> createWarehouse(
-      @RequestBody @Valid CreateWarehousePayloadDTO payloadDTO) {
-    Warehouse created =
-        warehouseService.createWarehouse(WarehouseMapper.INSTANCE.createDtoToModel(payloadDTO));
+    @PostMapping
+    public ResponseEntity<WarehouseResponseDTO> createWarehouse(
+            @RequestBody @Valid CreateWarehousePayloadDTO payloadDTO) {
+        Warehouse created =
+                warehouseService.createWarehouse(WarehouseMapper.INSTANCE.createDtoToModel(payloadDTO));
 
-    HttpHeaders headers = new HttpHeaders();
-    headers.add(
-        "Location",
-        linkTo(methodOn(WarehouseController.class).findById(created.getId()))
-            .withSelfRel()
-            .toString());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(
+                "Location",
+                linkTo(methodOn(WarehouseController.class).findById(created.getId()))
+                        .withSelfRel()
+                        .toString());
 
-    return assembler.toResponse(created, HttpStatus.CREATED, headers);
-  }
-
-  @GetMapping("/{id}")
-  public ResponseEntity<WarehouseResponseDTO> findById(@PathVariable Long id) {
-    Warehouse warehouse = warehouseService.findWarehouse(id);
-
-    return assembler.toResponse(warehouse, HttpStatus.CREATED, null);
-  }
-
-  @GetMapping("/fresh-products/duedate")
-  public /*ResponseEntity<List<BatchResponseDTO>>*/ ResponseEntity<List<Batch>> findDueDateBatches(
-      @RequestParam(name = "numb_days") String numberOfDays,
-      @RequestParam(name = "section_id") String sectionId) {
-    List<Batch> section =
-        warehouseService.dueDateBatches(Long.parseLong(numberOfDays), Long.parseLong(sectionId));
-    return ResponseEntity.ok(section);
-  }
-
-  @GetMapping("/fresh-products/duedate-batches")
-  public ResponseEntity<List<BatchResponseDTO>> findDueDateBatchesByCategory(
-      @RequestParam(name = "numb_days") String numberOfDays,
-      @RequestParam(name = "category") CategoryEnum category,
-      @RequestParam(name = "order") String order) {
-
-    List<Batch> batches =
-        warehouseService.dueDateBatchesByCategory(Long.parseLong(numberOfDays), category, order);
-    return batchAssembler.toCreatedResponse(batches);
-  }
-
-  @GetMapping("/fresh-products/list")
-  @JsonView(SectionView.SectionBatches.class)
-  public ResponseEntity<SectionBatchesDTO> listStockProducts(
-      @RequestParam(required = false) Long product,
-      @RequestParam(required = false, defaultValue = "L") SortTypeEnum sort,
-      Authentication authentication)
-      throws NotFoundException {
-    AppUser requestUser = (AppUser) authentication.getPrincipal();
-    Long managerId = requestUser.getId();
-
-    if (product == null) {
-      throw new IllegalArgumentException("Informe o ID do produto");
+        return assembler.toResponse(created, HttpStatus.CREATED, headers);
     }
 
-    List<Batch> batchProducts =
-        warehouseService.findProductOnManagerSection(managerId, product, sort);
+    @GetMapping("/{id}")
+    public ResponseEntity<WarehouseResponseDTO> findById(@PathVariable Long id) {
+        Warehouse warehouse = warehouseService.findWarehouse(id);
 
-    return sectionAssembler.toSectionBatchesResponse(batchProducts, product, HttpStatus.OK);
-  }
+        return assembler.toResponse(warehouse, HttpStatus.CREATED, null);
+    }
+
+    @GetMapping("/fresh-products/duedate")
+    public ResponseEntity<List<BatchResponseDTO>> findDueDateBatches(
+            @RequestParam(name = "numb_days") String numberOfDays,
+            @RequestParam(name = "section_id") String sectionId) {
+
+        List<Batch> batches =
+                warehouseService.dueDateBatches(Long.parseLong(numberOfDays), Long.parseLong(sectionId));
+        return batchAssembler.toCreatedResponse(batches);
+    }
+
+    @GetMapping("/fresh-products/duedate-batches")
+    public ResponseEntity<List<BatchResponseDTO>> findDueDateBatchesByCategory(
+            @RequestParam(name = "numb_days") String numberOfDays,
+            @RequestParam(name = "category") CategoryEnum category,
+            @RequestParam(name = "order") String order) {
+
+        List<Batch> batches =
+                warehouseService.dueDateBatchesByCategory(Long.parseLong(numberOfDays), category, order);
+        return batchAssembler.toCreatedResponse(batches);
+    }
+
+    @GetMapping("/fresh-products/list")
+    @JsonView(SectionView.SectionBatches.class)
+    public ResponseEntity<SectionBatchesDTO> listStockProducts(
+            @RequestParam(required = false) Long product,
+            @RequestParam(required = false, defaultValue = "L") SortTypeEnum sort,
+            Authentication authentication)
+            throws NotFoundException {
+        AppUser requestUser = (AppUser) authentication.getPrincipal();
+        Long managerId = requestUser.getId();
+
+        if (product == null) {
+            throw new IllegalArgumentException("Informe o ID do produto");
+        }
+
+        List<Batch> batchProducts =
+                warehouseService.findProductOnManagerSection(managerId, product, sort);
+
+        return sectionAssembler.toSectionBatchesResponse(batchProducts, product, HttpStatus.OK);
+    }
 }
