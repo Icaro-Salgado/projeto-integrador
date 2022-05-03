bea3cc541ce7321cb8de02c812ec9872d40ad6e9
pedroLSoares <pedro.levada@mercadolivre.com>
2022-05-03 14:36:49 -0300

    Add Swagger doc into categories controller

--- src/main/java/br/com/mercadolivre/projetointegrador/warehouse/controller/CategoryController.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/warehouse/controller/CategoryController.java
@@ -1,7 +1,16 @@
 package br.com.mercadolivre.projetointegrador.warehouse.controller;
 
+import br.com.mercadolivre.projetointegrador.warehouse.docs.config.SecuredWarehouseRestController;
+import br.com.mercadolivre.projetointegrador.warehouse.dto.response.BatchResponseDTO;
 import br.com.mercadolivre.projetointegrador.warehouse.dto.response.CategoryListDTO;
 import br.com.mercadolivre.projetointegrador.warehouse.enums.CategoryEnum;
+import br.com.mercadolivre.projetointegrador.warehouse.exception.ErrorDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +22,24 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/fresh-products/categories")
-public class CategoryController {
+@Tag(name = "[Warehouse] - Categories")
+public class CategoryController implements SecuredWarehouseRestController {
 
+    @Operation(
+            summary = "LISTA AS CATEGORIAS CADASTRADAS")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Ordem criada",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = CategoryListDTO.class))
+                            }),
+            })
     @GetMapping
-    public ResponseEntity<?> listCategories(){
+    public ResponseEntity<List<CategoryListDTO>> listCategories(){
         List<CategoryListDTO> categories = Arrays.stream(CategoryEnum.values()).map(val -> new CategoryListDTO(val.label(), val.name())).collect(Collectors.toList());
 
         return ResponseEntity.ok(categories);
