907f4a0db646a87e1a7464ac86bedcbe6ae686a3
Maran BR <maran.morimoto@mercadolivre.com>
2022-05-05 15:51:33 -0300

    due-date-batches methods and tests

--- src/main/java/br/com/mercadolivre/projetointegrador/security/configuration/SecurityConfiguration.java
+++ src/main/java/br/com/mercadolivre/projetointegrador/security/configuration/SecurityConfiguration.java
@@ -23,49 +23,53 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-  private final AuthenticationService authenticationService;
-  private final TokenService tokenService;
-  private final AppUserRepository repository;
-  private final PasswordEncoder passwordEncoder;
+    private final AuthenticationService authenticationService;
+    private final TokenService tokenService;
+    private final AppUserRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-  @Override
-  @Bean
-  protected AuthenticationManager authenticationManager() throws Exception {
-    return super.authenticationManager();
-  }
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.userDetailsService(authenticationService).passwordEncoder(new BCryptPasswordEncoder());
-    auth.inMemoryAuthentication()
-        .passwordEncoder(passwordEncoder)
-        .withUser("springTest")
-        .password(passwordEncoder.encode("usertest"))
-        .roles("USER");
-  }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(authenticationService).passwordEncoder(new BCryptPasswordEncoder());
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder)
+                .withUser("springTest")
+                .password(passwordEncoder.encode("usertest"))
+                .roles("USER");
+    }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http = http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and();
 
-    http.cors()
-        .and()
-        .csrf()
-        .disable()
-        .authorizeRequests()
-        .antMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
-        .permitAll()
-        .antMatchers(HttpMethod.POST, "/api/v1/auth", "/api/v1/*/auth/register")
-        .permitAll()
-        .antMatchers("/api/v1/marketplace/**")
-        .hasAuthority("CUSTOMER")
-        .antMatchers("/api/v1/warehouse/**")
-        .access("hasAuthority('MANAGER')")
-        .anyRequest()
-        .authenticated()
-        .and()
-        .addFilterBefore(
-            new TokenAuthenticationFilter(tokenService, repository),
-            UsernamePasswordAuthenticationFilter.class);
-  }
+        http.cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/api/v1/fresh-products/due-date/queryparam=7queryparam=ff")
+                .permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/auth", "/api/v1/*/auth/register")
+                .permitAll()
+                .antMatchers("/api/v1/marketplace/**")
+                .hasAuthority("CUSTOMER")
+                .antMatchers("/api/v1/warehouse/**")
+                .access("hasAuthority('MANAGER')")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .addFilterBefore(
+                        new TokenAuthenticationFilter(tokenService, repository),
+                        UsernamePasswordAuthenticationFilter.class);
+    }
 }
